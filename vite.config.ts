fc8fb32af10621881ee4e76e7c703d540dd60ab1
ndom91 <yo@ndo.dev>
2020-11-21 21:52:20 +0100

    chore: formatting / license

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import path from 'path'
 
 module.exports = {
-    alias: {
-        '/@/': path.resolve(__dirname, './src'),
-        '/@views/': path.resolve(__dirname, './src/views'),
-        '/@components/': path.resolve(__dirname, './src/components'),
-        '/@utils/': path.resolve(__dirname, './src/utils'),
-    },
-    optimizeDeps: {
-        include: ["lodash"]
-    },
-    proxy: {
-        '/api': {
-            target: 'http://jsonplaceholder.typicode.com',
-            changeOrigin: true,
-            rewrite: (path: string) => path.replace(/^\/api/, '')
-        }
+  alias: {
+    '/@/': path.resolve(__dirname, './src'),
+    '/@views/': path.resolve(__dirname, './src/views'),
+    '/@components/': path.resolve(__dirname, './src/components'),
+    '/@utils/': path.resolve(__dirname, './src/utils')
+  },
+  optimizeDeps: {
+    include: ['lodash']
+  },
+  proxy: {
+    '/api': {
+      target: 'http://jsonplaceholder.typicode.com',
+      changeOrigin: true,
+      rewrite: (path: string) => path.replace(/^\/api/, '')
     }
-    // ...
+  }
+  // ...
 }
