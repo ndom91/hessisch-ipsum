16783a8be20fb08042d682485d4c52af4af59f8d
ndom91 <yo@ndo.dev>
2021-02-21 16:19:54 +0100

    style: colors

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,11 @@
   <div class="mt-8 mb-6 flex flex-col space-y-4 align-center">
     <div class="text-4xl font-sans font-thin text-center">
       <div>Lorem Ipsum Generator</div>
-      <div class="text-2xl italic">aber uff Hessisch!</div>
+      <div class="text-red-500 text-2xl italic">
+        <span class="text-gray-800">aber </span>
+        <span class="text-red-500">uff </span>
+        <span class="text-yellow-500">Hessisch!</span>
+      </div>
     </div>
   </div>
 </template>
